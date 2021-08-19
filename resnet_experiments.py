09b58463d5fe10fb8395db4d18d1bcca4d557364
yanneta <yannet@gmail.com>
2021-08-19 12:07:23 -0700

    fixing experiments for mura

--- resnet_experiments.py
+++ resnet_experiments.py
@@ -20,6 +20,9 @@ lrs = {"chexpert": 0.001, "mura": 0.02, "rsna": 0.1}
 num_classes_dict = {"chexpert": 5, "mura":1, "rsna": 1}
 lr = lrs[dataset]
 num_classes = num_classes_dict[dataset]
+binary = True
+if dataset == "chexpert":
+    binary = False
 
 widths = [1.0, 0.75, 0.5, 0.25]
 depths = [[[[64, 2], [128, 2]], [[256, 2], [512, 1]]],
@@ -32,11 +35,14 @@ depths = [[[[64, 2], [128, 2]], [[256, 2], [512, 1]]],
 
 data = []
 
+
+total = len(widths)*len(depths)*3
+counter = 0
 for w in widths:
     for d in depths:
         for seed in range(1, 4):
             d_s = sum(j[1] for i in d for j in i)
-            print('width multiplier - %.3f depth multiplier - %.3f' % (w, d_s))
+            print('width multiplier - %.3f depth multiplier - %.3f, percent done %.1f' % (w, d_s, 100*counter/total))
             model = resnet18(num_classes=num_classes, width_mult=w, 
                     inverted_residual_setting1=d[0], 
                     inverted_residual_setting2=d[1]).cuda()
@@ -45,8 +51,8 @@ for w in widths:
             score, t = train_triangular_policy(model, optimizer,
                     train_loader, valid_loader, valid_dataset,
                     loss_fn=F.binary_cross_entropy_with_logits, 
-                    dataset=dataset, binary=False, max_lr=lr, epochs=15)
-        
+                    dataset=dataset, binary=binary, max_lr=lr, epochs=15)
+            counter += 1
             data.append([w, d_s, score, p, t])
 
 
